252db9a0f4f0c10fc32f29a26428d2dddb3e3750
weidermartenn <weidermartenn@gmail.com>
2024-10-17 22:20:21 +0300

    asdasd

--- fxspot_app/src/widgets/CyContainer/ui/CyContainer.vue
+++ fxspot_app/src/widgets/CyContainer/ui/CyContainer.vue
@@ -1,13 +1,39 @@
 <template>
-    <div class="flex basis-2/3 md:basis-5/6 lg:basis-1/2 h-[600px] bg-semiaccent rounded-md inset-0 mx-auto shadow-lg shadow-perfectgreen overflow-x-scroll snap-mandatory">
-
+    <div class="flex basis-2/3 md:basis-5/6 lg:basis-1/2 h-[600px] bg-semiaccent rounded-md inset-0 mx-auto shadow-lg shadow-perfectgreen overflow-x-scroll snap-x snap-mandatory custom-scroll">
+        <div class="snap-center min-w-full h-full shrink-0 bg-amber-200"></div>
+        <div class="snap-center min-w-full h-full shrink-0 bg-emerald-500"></div>
+        <div class="snap-center min-w-full h-full shrink-0 bg-sky-700"></div>
     </div>
-</template>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
+  <style scoped>
+  .custom-scroll::-webkit-scrollbar {
+    width: 12px;
+    height: 16px; 
+  }
+  
+  .custom-scroll::-webkit-scrollbar-track {
+    background-color: #2e2e2e; 
+    border-radius: 10px;
+  }
+  
+  .custom-scroll::-webkit-scrollbar-thumb {
+    background-color: #80d977;
+    border-radius: 10px;
+    border: 3px solid transparent; 
+    background-clip: padding-box;
+  }
+  
+  .custom-scroll::-webkit-scrollbar-thumb:hover {
+    background-color: #66c764;
+  }
 
-<script setup lang="ts">
-
-</script>
-
-<style scoped>
-
-</style>
+  .custom-scroll {
+    scrollbar-width: 20px;
+    scrollbar-color: #80d977 #2e2e2e;
+  }
+  </style>
+  
--- fxspot_app/src/widgets/Header/ui/HeaderView.vue
+++ fxspot_app/src/widgets/Header/ui/HeaderView.vue
@@ -1,9 +1,15 @@
 <template>
-    <div class="sticky flex gap-6 items-center top-0 py-4 px-6 w-full h-[75px] bg-main border-b-2 border-semiaccent">
-        <router-link to="/">
-            <p class="text-3xl text-white">FXSpot</p>
-        </router-link>
-        <UserLoc />
+    <div class="sticky flex justify-between gap-6 top-0 py-4 px-6 w-full h-[75px] bg-main border-b-2 border-semiaccent">
+        <div class="flex flex-row items-center gap-4">
+            <router-link to="/">
+                <p class="text-3xl text-white">FXSpot</p>
+            </router-link>
+            <UserLoc />
+        </div>
+        <div class="flex flex-row items-center gap-4">
+            <a class="fa-brands fa-github text-3xl text-white" href="https://github.com/weidermartenn/exchange-app" target="_blank"></a>
+            <i class="fa-solid fa-microchip text-xl text-white">apis</i>
+        </div>
     </div>
 </template>
 
